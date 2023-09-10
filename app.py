ed967cac56c7461fa502c96f1f8a315d2f2227cf
Юстиниан Шишкин <mister.yustinian@yandex.ru>
2023-09-11 02:10:18 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Шишкин Юстиниан Михайлович, лабораторная 1t</title>
+</head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        <header>
+
+        <h1>web-сервер на flask<h1>
+
+        <footer>
+            &copy: Юстиниан Шишкин, ФБИ-14, 3 курс, 2023
+        <footer>
+    </body>
+</html>
+"""
+
