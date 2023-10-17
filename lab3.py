91dfe114ab199086acad250f2bdd1c435908d6de
Юстиниан Шишкин <mister.yustinian@yandex.ru>
2023-10-17 20:22:29 +0700

    Попука билета и корректировки

--- lab3.py
+++ lab3.py
@@ -17,3 +17,52 @@ def form1():
         errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template("form1.html", user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else: 
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    return render_template('pay.html', price=price)
+
+@lab3.route('/lab3/succes')
+def succes():
+    return render_template('succes.html')
+
+
+@lab3.route('/lab3/tiket')
+def tiketpay():
+    return render_template('tiket.html')
+
+@lab3.route('/lab3/tiketpay')
+def tiket():
+    pricetiket = 0
+    tiket = request.args.get('tiket')
+    if tiket == 'детский':
+        pricetiket = 1000
+    else:
+        pricetiket = 1800
+
+    if request.args.get('baggage') == 'on':
+        pricetiket += 600
+    return render_template('tiketpay.html', pricetiket=pricetiket)
+
+
+@lab3.route('/lab3/succestiket')
+def succestiket():
+    return render_template('succestiket.html')
