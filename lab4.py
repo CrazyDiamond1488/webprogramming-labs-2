19e90b16ec4d3d35a4995df353f64c2fbdeb4c9e
Юстиниан Шишкин <mister.yustinian@yandex.ru>
2023-10-22 01:32:25 +0700

    Заказ зерна

--- lab4.py
+++ lab4.py
@@ -28,14 +28,14 @@ def login():
 
 
 @lab4.route('/lab4/freeze', methods=['GET', 'POST'])
-def lab4_freeze():
+def freeze():
     if request.method == 'GET':
         return render_template("freeze.html")
     
     temperature = request.form.get('temperature')
     if  not temperature:
         result = 'ошибка: не задана температура!'
-    elif int(temperature) < -12:
+    elif int(temperature) <= -12:
         result = 'не удалось установить температуру — слишком низкое значение'
     elif int(temperature) > -1:
         result = 'не удалось установить температуру — слишком высокое значение'
@@ -47,3 +47,41 @@ def lab4_freeze():
         result = f"Установлена температура: {temperature}°C *"
 
     return render_template("freeze.html", result=result)
+
+@lab4.route('/lab4/zerno',  methods=['GET', 'POST'])
+def zerno():
+    if request.method == 'GET':
+        return render_template("zerno.html")
+    
+    zerno = request.form.get('zerno')
+    weight = int(request.form.get('weight'))
+    price = 0
+    
+    if zerno == 'ячмень':
+        price = 12000
+    elif zerno == 'овёс':
+        price = 8500
+    elif zerno == 'пшеница':
+        price = 8700
+    elif zerno == 'рожь':
+        price = 14000
+    
+    if weight <= 0:
+        errors = 'Не верно указан объём закупки'
+        return render_template("zerno.html", errors=errors)
+    elif weight >= 500:
+        errors = 'Извините, но такого количества зерна, пока что нет в наличии'
+        return render_template("zerno.html", errors=errors)
+    
+    if weight > 50:
+        total_price = (weight * price) * 0.9
+        message = f"Заказ успешно сформирован. Вы заказали {zerno}. Вес: {weight} т. " \
+                  f"Сумма к оплате: {total_price} руб. (Применена скидка за большой объем)"
+    else:
+        total_price = weight * price
+        message = f"Заказ успешно сформирован. Вы заказали {zerno}. Вес: {weight} т. " \
+                  f"Сумма к оплате: {total_price} руб."
+        
+    return render_template('zerno.html', message=message)
+    
+
