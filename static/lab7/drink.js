9be4a731c9125af01417ec0eece5bf4a8706c0cc
Юстиниан Шишкин <mister.yustinian@yandex.ru>
2023-12-04 14:04:11 +0700

    Возврат оплаты

--- static/lab7/drink.js
+++ static/lab7/drink.js
@@ -65,4 +65,43 @@ function pay() {
         });
 }
 
+function cancel_pay() {
+    const drink = document.querySelector('[name=drink]:checked').value;
+    const milk = document.querySelector('[name=milk]').checked;
+    const sugar = document.querySelector('[name=sugar]').checked;
+    const card = document.querySelector('[name=card]').value;
+    const name = document.querySelector('[name=name]').value;
+    const cvv = document.querySelector('[name=cvv]').value;
+
+    const obj = {
+        "method": "pay",
+        "params": {
+            drink: drink,
+            milk: milk,
+            sugar: sugar,
+            card: card,
+            name: name,
+            cvv: cvv
+        }
+    };
+
+    fetch('/lab7/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+        if (data.error) {
+            document.querySelector('#pay-status').innerHTML = `Ошибка: ${data.error}`;
+        } else {
+            document.querySelector('#pay-status').innerHTML = 'Возврат средств прошло успешно на карту:' + card;
+            document.querySelector('#pay-status').style.display = 'block';
+        }
+    });
+}
 
